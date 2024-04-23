861b474f56f19f0a3cf5340f50839e4bc5d213d9
Asif Istiaque <asifistiaque.ai@gmail.com>
2024-04-24 03:07:32 +0600

    ds

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from '@/components/library/providers/Providers';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +14,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
+			<Head>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1, user-scalable=yes'
+				/>
+			</Head>
 			<body className={inter.className}>
 				<Providers>{children}</Providers>
 			</body>
