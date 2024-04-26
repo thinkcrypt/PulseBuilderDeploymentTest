41e609ec929397723dab87d4b240c447ef249b44
Asif Istiaque <asifistiaque.ai@gmail.com>
2024-04-26 23:02:10 +0600

    leave management

--- src/app/leave-request/success/page.tsx
+++ src/app/leave-request/success/page.tsx
@@ -1,42 +1,41 @@
 'use client';
-import Success from '@/components/attendance/Success';
+
 import React, { Suspense } from 'react';
+import Success from '@/components/attendance/Success';
 import { Text, Stack, Flex, Center } from '@chakra-ui/react';
 import { useSearchParams } from 'next/navigation';
 
-const SuccessPage = () => {
+const SuccessComponent = () => {
 	const searchParams = useSearchParams();
 	const trackingId = searchParams.get('tid');
 
 	return (
-		<Suspense
-			fallback={
-				<Center
-					flex={1}
-					w='full'
-					h='100vh'>
-					...Processing
-				</Center>
-			}>
-			<Success
-				title='Submitted'
-				flex={1}>
-				<Center>
-					<Stack
-						mb={24}
-						textAlign='center'
-						fontSize='18px'>
-						<Text>Leave request submitted successfully</Text>
-						<Text>Your Tracking Id is: {trackingId}</Text>
-					</Stack>
-				</Center>
+		<Success
+			title='Submitted'
+			flex={1}>
+			<Center>
+				<Stack
+					mb={24}
+					textAlign='center'
+					fontSize='18px'>
+					<Text>Leave request submitted successfully</Text>
+					<Text>Your Tracking Id is: {trackingId}</Text>
+				</Stack>
+			</Center>
 
-				<Flex
-					flex={0}
-					justify='flex-end'>
-					<Text>Thank You</Text>
-				</Flex>
-			</Success>
+			<Flex
+				flex={0}
+				justify='flex-end'>
+				<Text>Thank You</Text>
+			</Flex>
+		</Success>
+	);
+};
+
+const SuccessPage = () => {
+	return (
+		<Suspense fallback={<div>Loading...</div>}>
+			<SuccessComponent />
 		</Suspense>
 	);
 };
