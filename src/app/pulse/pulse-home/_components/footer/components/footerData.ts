1a4cf100fd7361a81d9b7decae421290374af655
NaimulHaqAbeer <naimulhaqabeer.thinkcrypt@gmail.com>
2025-01-01 18:00:20 +0600

    small change

--- src/app/pulse/pulse-home/_components/footer/components/footerData.ts
+++ src/app/pulse/pulse-home/_components/footer/components/footerData.ts
@@ -1,57 +1,57 @@
 export const footerData = {
-	footer: {
-		support: {
-			title: 'Support',
-			card: [
-				{
-					topText: '9AM - 8PM',
-					bottomText: '16793',
-					icon: 'phone',
-					borderColor: '#ddd',
-				},
-				{
-					topText: 'Store Location',
-					bottomText: 'Find Our Support',
-					icon: 'location',
-				},
-			],
-		},
-		about: {
-			title: 'About Us',
-			links: [
-				{
-					text: 'Privacy Policy',
-					link: '/privacy-policy',
-				},
-				{
-					text: 'Contact Us',
-					link: '/contact-us',
-				},
-				{
-					text: 'About Us',
-					link: '/about',
-				},
-				{
-					text: 'Termns and Condition',
-					link: '/terms',
-				},
-			],
-		},
-		connected: {
-			title: 'Stay Connected',
-			address: {
-				brand: 'Thinkcrypt.IO',
-				office:
-					'Flat 5B, House \n88 Road 17/A, Block E, Banani \n Dhaka 1213, Bangladesh',
-				email: 'info@thinkcrypt.io',
-				phone: '(+88) 01828398225, 01799399555',
-			},
-		},
-		copyright: {
-			leftText: 'powered by ',
-			thinkText: 'thinkcrypt.io ',
-			link: 'https://thinkcrypt.io',
-			rightText: '©2024-2025 | all right reserved',
-		},
-	},
+  footer: {
+    support: {
+      title: "Support",
+      card: [
+        {
+          topText: "9AM - 8PM",
+          bottomText: "16793",
+          icon: "phone",
+          borderColor: "#ddd",
+        },
+        {
+          topText: "Store Location",
+          bottomText: "Find Our Support",
+          icon: "location",
+        },
+      ],
+    },
+    about: {
+      title: "About Us",
+      links: [
+        {
+          text: "Privacy Policy",
+          link: "/privacy-policy",
+        },
+        {
+          text: "Contact Us",
+          link: "/contact-us",
+        },
+        {
+          text: "About Us",
+          link: "/about",
+        },
+        {
+          text: "Terms and Condition",
+          link: "/terms",
+        },
+      ],
+    },
+    connected: {
+      title: "Stay Connected",
+      address: {
+        brand: "Thinkcrypt.IO",
+        office:
+          "Flat 5B, House \n88 Road 17/A, Block E, Banani \n Dhaka 1213, Bangladesh",
+        email: "info@thinkcrypt.io",
+        phone: "(+88) 01828398225, 01799399555",
+      },
+    },
+    copyright: {
+      leftText: "powered by ",
+      thinkText: "thinkcrypt.io ",
+      link: "https://thinkcrypt.io",
+      rightText: "©2024-2025 | all right reserved",
+    },
+  },
 };
