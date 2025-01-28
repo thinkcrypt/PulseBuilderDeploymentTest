f671e55c62970e892a37f1b48aabcf20b34fc130
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-01-28 12:59:37 +0600

    zhoeid

---
+++ src/app/theme/zhoei/layout.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Builder | Zhoei Theme',
+};
+
+export default function Page({ children }: { children: React.ReactNode }) {
+	return children;
+}
