81f204c66b012661d57ab94dba93c2d5081da04e
Asif Istiaque <asifistiaque.ai@gmail.com>
2025-01-29 15:58:08 +0600

    s

--- src/app/theme/pulse/_components/Banner/pulseHeroData.tsx
+++ src/app/theme/pulse/_components/Banner/pulseHeroData.tsx
@@ -1,27 +1,27 @@
 const pulseHeroData = [
-  {
-    sectionTitle: "Hero Section",
-    name: "hero.images",
-    label: "Hero Images",
-    type: "image-array",
-  },
-  {
-    name: "hero.height",
-    label: "Hero Height",
-    type: "number",
-    endOfSection: true,
-  },
+	{
+		sectionTitle: 'Hero Section',
+		name: 'hero.images',
+		label: 'Hero Images',
+		type: 'image-array',
+	},
+	{
+		name: 'hero.height',
+		label: 'Hero Height',
+		type: 'number',
+		endOfSection: true,
+	},
 
-  {
-    sectionTitle: "Show/Hide",
-    name: "hero.hide",
-    label: "Show/Hide Banner",
-    type: "select",
-    options: [
-      { label: "Show", value: false },
-      { label: "Hide", value: true },
-    ],
-  },
+	{
+		sectionTitle: 'Show/Hide',
+		name: 'hero.hide',
+		label: 'Show/Hide Banner',
+		type: 'select',
+		options: [
+			{ label: 'Show', value: false },
+			{ label: 'Hide', value: true },
+		],
+	},
 ];
 
 export default pulseHeroData;
